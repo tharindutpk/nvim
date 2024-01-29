b7d82aa08a89b008810068cecced3d19a9bcc55f
Tharindu Pramuditha <tharindutpk@gmail.com>
2024-01-29 16:20:39 +0530

    Changes to the LSP settings

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -124,67 +124,19 @@ return {
   },
 
   {
-    'hrsh7th/nvim-cmp',
-    event = { 'InsertEnter' },
+    'glepnir/lspsaga.nvim',
+    event = { 'BufReadPost', 'BufNewFile', 'BufWritePre' },
     dependencies = {
-      -- Adds LSP completion capabilities
-      'hrsh7th/cmp-nvim-lsp',
-      'hrsh7th/cmp-path',
-
-      -- Snippet Engine & its associated nvim-cmp source
-      'L3MON4D3/LuaSnip',
-      'saadparwaiz1/cmp_luasnip',
+      { 'nvim-tree/nvim-web-devicons' },
+      { 'nvim-treesitter/nvim-treesitter' },
     },
-    opts = {},
-    config = function()
-      local luasnip = require('luasnip')
-      local cmp = require('cmp')
-      require('luasnip.loaders.from_vscode').lazy_load()
-      luasnip.config.setup({})
-      require('cmp').setup({
-        snippet = {
-          expand = function(args)
-            luasnip.lsp_expand(args.body)
-          end,
-        },
-        completion = {
-          completeopt = 'menu,menuone,noinsert',
-        },
-        mapping = cmp.mapping.preset.insert({
-          ['<C-n>'] = cmp.mapping.select_next_item(),
-          ['<C-p>'] = cmp.mapping.select_prev_item(),
-          ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-          ['<C-f>'] = cmp.mapping.scroll_docs(4),
-          ['<C-Space>'] = cmp.mapping.complete({}),
-          ['<CR>'] = cmp.mapping.confirm({
-            behavior = cmp.ConfirmBehavior.Replace,
-            select = true,
-          }),
-          ['<Tab>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_next_item()
-            elseif luasnip.expand_or_locally_jumpable() then
-              luasnip.expand_or_jump()
-            else
-              fallback()
-            end
-          end, { 'i', 's' }),
-          ['<S-Tab>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_prev_item()
-            elseif luasnip.locally_jumpable(-1) then
-              luasnip.jump(-1)
-            else
-              fallback()
-            end
-          end, { 'i', 's' }),
-        }),
-        sources = {
-          { name = 'nvim_lsp' },
-          { name = 'luasnip' },
-          { name = 'path' },
-        },
-      })
-    end,
+    opts = {
+      lightbulb = {
+        enable = false,
+      },
+      symbol_in_winbar = {
+        show_file = false,
+      },
+    },
   },
 }
