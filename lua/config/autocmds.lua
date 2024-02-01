1fd407d6c728d95bd1c9d17e7168f84c288d5cc3
Tharindu Pramuditha <tharindutpk@gmail.com>
2024-02-01 18:30:33 +0530

    Updated autocmds to include last location of a file

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,9 +1,30 @@
+local function augroup(name)
+  return vim.api.nvim_create_augroup('tharindutpk_' .. name, { clear = true })
+end
+
 -- Highlight on yank
-local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
 vim.api.nvim_create_autocmd('TextYankPost', {
+  group = augroup('yank_highlight'),
+  pattern = '*',
   callback = function()
     vim.highlight.on_yank()
   end,
-  group = highlight_group,
-  pattern = '*',
+})
+
+-- Go to last location when opening a buffer
+vim.api.nvim_create_autocmd('BufReadPost', {
+  group = augroup('last_location'),
+  callback = function(event)
+    local exclude = { 'gitcommit' }
+    local buf = event.buf
+    if vim.tbl_contains(exclude, vim.bo[buf].filetype) or vim.b[buf].lazyvim_last_loc then
+      return
+    end
+    vim.b[buf].lazyvim_last_loc = true
+    local mark = vim.api.nvim_buf_get_mark(buf, '"')
+    local lcount = vim.api.nvim_buf_line_count(buf)
+    if mark[1] > 0 and mark[1] <= lcount then
+      pcall(vim.api.nvim_win_set_cursor, 0, mark)
+    end
+  end,
 })
