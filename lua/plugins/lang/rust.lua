1aa2247b0537365d1feb8a7bacc016e86bf13c5d
Tharindu Pramuditha <tharindutpk@gmail.com>
2024-02-01 20:11:40 +0530

    Improved rust support

--- lua/plugins/lang/rust.lua
+++ lua/plugins/lang/rust.lua
@@ -3,7 +3,7 @@ return {
     'nvim-treesitter/nvim-treesitter',
     opts = function(_, opts)
       if type(opts.ensure_installed) == 'table' then
-        vim.list_extend(opts.ensure_installed, { 'rust' })
+        vim.list_extend(opts.ensure_installed, { 'rust', 'toml' })
       end
     end,
   },
@@ -12,8 +12,50 @@ return {
     'neovim/nvim-lspconfig',
     opts = {
       servers = {
-        rust_analyzer = {},
+        rust_analyzer = {
+          ['rust-analyzer'] = {
+            diagnostics = {
+              enable = false,
+            },
+            cargo = {
+              allFeatures = true,
+              loadOutDirsFromCheck = true,
+              runBuildScripts = true,
+            },
+            -- Add clippy lints for Rust.
+            checkOnSave = {
+              allFeatures = true,
+              command = 'clippy',
+              extraArgs = { '--no-deps' },
+            },
+          },
+        },
       },
     },
+  },
+
+  {
+    'hrsh7th/nvim-cmp',
+    dependencies = {
+      {
+        'Saecki/crates.nvim',
+        event = { 'BufRead Cargo.toml' },
+        opts = {
+          src = {
+            cmp = { enabled = true },
+          },
+        },
+      },
+    },
+    opts = function(_, opts)
+      local cmp = require('cmp')
+      vim.api.nvim_create_autocmd('BufRead', {
+        group = vim.api.nvim_create_augroup('CmpSourceCargo', { clear = true }),
+        pattern = 'Cargo.toml',
+        callback = function()
+          cmp.setup.buffer({ sources = { { name = 'crates' } } })
+        end,
+      })
+    end,
   },
 }
