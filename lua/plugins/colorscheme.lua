aaee236547df1e0597fb67aef5db832711c0e8ed
Tharindu Pramuditha <tharindutpk@gmail.com>
2024-02-02 00:01:08 +0530

    Changed telescope highlights in catppuccin theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -6,30 +6,29 @@ return {
     config = function()
       require('catppuccin').setup({
         flavour = 'mocha', -- latte, frappe, macchiato, mocha
-        dim_inactive = {
-          enabled = false, -- dims the background color of inactive window
-          shade = 'dark',
-          percentage = 0.15, -- percentage of the shade to apply to the inactive window
-        },
-        no_italic = false, -- Force no italic
-        no_bold = true, -- Force no bold
-        no_underline = false, -- Force no no_underline
-        styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
-          comments = { 'italic' }, -- Change the style of comments
-          conditionals = { 'italic' },
-          loops = {},
-          functions = {},
-          keywords = {},
-          strings = {},
-          variables = {},
-          numbers = {},
-          booleans = {},
-          properties = {},
-          types = {},
-          operators = {},
-        },
         color_overrides = {},
         custom_highlights = {},
+        highlight_overrides = {
+          mocha = function(mocha)
+            return {
+              -- Telescope highlights
+              TelescopeBorder = { fg = mocha.green },
+              TelescopeSelectionCaret = { fg = mocha.flamingo },
+              TelescopeSelection = { fg = mocha.text, bg = mocha.surface2, style = { 'bold' } },
+              TelescopeMatching = { fg = mocha.peach },
+              TelescopePromptPrefix = { fg = mocha.text, bg = mocha.none },
+              TelescopePromptNormal = { bg = mocha.crust },
+              TelescopeResultsNormal = { bg = mocha.crust },
+              TelescopePreviewNormal = { bg = mocha.crust },
+              TelescopePromptBorder = { bg = mocha.crust, fg = mocha.mantle },
+              TelescopeResultsBorder = { bg = mocha.crust, fg = mocha.mantle },
+              TelescopePreviewBorder = { bg = mocha.crust, fg = mocha.mantle },
+              TelescopePromptTitle = { fg = mocha.text, bg = mocha.none },
+              TelescopeResultsTitle = { fg = mocha.text, bg = mocha.none },
+              TelescopePreviewTitle = { fg = mocha.text, bg = mocha.none },
+            }
+          end,
+        },
         integrations = {
           alpha = true,
           cmp = true,
