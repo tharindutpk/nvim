c45a2f2951125cbb7162a4113599d2f684166354
Tharindu Pramuditha <tharindutpk@gmail.com>
2024-02-12 22:47:39 +0530

    Improved null-ls loading time

--- lua/plugins/format.lua
+++ lua/plugins/format.lua
@@ -1,18 +1,7 @@
 return {
-  -- {
-  --   'stevearc/conform.nvim',
-  --   event = { 'BufWritePre' },
-  --   cmd = { 'ConformInfo' },
-  --   opts = {
-  --     formatters_by_ft = {
-  --       lua = { 'stylua' },
-  --     },
-  --     format_on_save = { timeout_ms = 500, lsp_fallback = false },
-  --   },
-  -- },
-
   {
     'nvimtools/none-ls.nvim',
+    event = { 'BufReadPost', 'BufNewFile', 'BufWritePre' },
     opts = function(_, opts)
       local null_ls = require('null-ls')
       opts.sources = vim.list_extend(opts.sources or {}, {
