98ee2911eb02a9a3e9aebd1496bc1005a8e5f224
Tharindu Pramuditha <tharindutpk@gmail.com>
2024-02-14 23:52:29 +0530

    Changed gitsigns symbols

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -2,6 +2,14 @@ return {
   {
     'lewis6991/gitsigns.nvim',
     event = { 'BufReadPost', 'BufNewFile', 'BufWritePre' },
-    opts = {},
+    opts = {
+      signs = {
+        add = { text = '+' },
+        change = { text = '~' },
+        delete = { text = '_' },
+        topdelete = { text = '‾' },
+        changedelete = { text = '~' },
+      },
+    },
   },
 }
