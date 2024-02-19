22527b29911730583a378a586d9ecde4bb1e7c87
Tharindu Pramuditha <tharindutpk@gmail.com>
2024-02-20 00:16:25 +0530

    Added trouble.nvim and todo-comments.nvim

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -6,6 +6,18 @@ return {
   },
 
   {
+    'folke/trouble.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    opts = {},
+  },
+
+  {
+    'folke/todo-comments.nvim',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    opts = {},
+  },
+
+  {
     'lukas-reineke/indent-blankline.nvim',
     event = { 'BufReadPost', 'BufNewFile', 'BufWritePre' },
     main = 'ibl',
