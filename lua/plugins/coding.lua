30c69f34f5f9e99cf347ce8de1a205d983ec0e2f
Tharindu Pramuditha <tharindutpk@gmail.com>
2024-02-26 22:13:13 +0530

    Added nvim surround

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -31,6 +31,17 @@ return {
   },
 
   {
+    'kylechui/nvim-surround',
+    version = '*', -- Use for stability; omit to use `main` branch for the latest features
+    event = 'VeryLazy',
+    config = function()
+      require('nvim-surround').setup({
+        -- Configuration here, or leave empty to use defaults
+      })
+    end,
+  },
+
+  {
     'rmagatti/alternate-toggler',
     event = { 'BufReadPost', 'BufNewFile', 'BufWritePre' },
     keys = {
