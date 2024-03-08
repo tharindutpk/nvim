6b38b466573deb8a4010fd3fddd1c527da7e798f
Tharindu Pramuditha <tharindutpk@gmail.com>
2024-03-08 23:57:40 +0530

    Added keymaps for clearing highlights

--- lua/config/mappings.lua
+++ lua/config/mappings.lua
@@ -1,3 +1,6 @@
 -- [[ Basic Keymaps ]]
 vim.keymap.set('i', 'jk', '<ESC>')
 vim.keymap.set('t', 'jk', '<C-\\><C-n>')
+
+-- Clear highlight on pressing <Esc> in normal mode
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
