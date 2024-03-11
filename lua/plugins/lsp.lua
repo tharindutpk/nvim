ca44d2f7265ef3299ec9e0e3f504d1eda95c0d4a
Tharindu Pramuditha <tharindutpk@gmail.com>
2024-03-11 22:31:52 +0530

    Tiny changes for plugins

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -2,7 +2,9 @@ return {
   {
     'williamboman/mason.nvim',
     cmd = 'Mason',
-    keys = { { '<leader>cm', '<cmd>Mason<cr>', desc = 'Mason' } },
+    keys = {
+      { '<leader>cm', '<cmd>Mason<cr>', desc = 'Mason' },
+    },
     build = ':MasonUpdate',
     opts = {
       ensure_installed = {
