198eaaf8ef8f8ea9bf66ac0e581d8db92b705261
Tharindu Pramuditha <tharindutpk@gmail.com>
2024-03-14 22:51:57 +0530

    Added nvim-lint

--- lua/plugins/format.lua
+++ lua/plugins/format.lua
@@ -1,7 +1,7 @@
 return {
   {
     'stevearc/conform.nvim',
-    event = { 'BufWritePre' },
+    event = { 'BufReadPost', 'BufNewFile', 'BufWritePre' },
     cmd = { 'ConformInfo' },
     opts = {
       formatters_by_ft = {
@@ -9,5 +9,16 @@ return {
       },
       format_on_save = { timeout_ms = 500, lsp_fallback = false },
     },
+  },
+
+  {
+    'mfussenegger/nvim-lint',
+    event = { 'BufReadPost', 'BufNewFile', 'BufWritePre' },
+    opts = {
+      linters_by_ft = {
+        lua = { 'luacheck' },
+      },
+    },
+    config = function() end,
   },
 }
