f0f73b04082f8d02126845156b5d488328ef453e
Tharindu Pramuditha <tharindutpk@gmail.com>
2024-07-07 12:14:08 +0530

    Added autoformat toggle to work with conform

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -29,3 +29,24 @@ vim.api.nvim_create_autocmd('BufReadPost', {
     end
   end,
 })
+
+-- Format disable toggle to disable autoformat
+vim.api.nvim_create_user_command('FormatDisable', function(args)
+  if args.bang then
+    -- FormatDisable! will disable formatting just for this buffer
+    vim.b.disable_autoformat = true
+  else
+    vim.g.disable_autoformat = true
+  end
+end, {
+  desc = 'Disable autoformat-on-save',
+  bang = true,
+})
+
+-- Format enable toggle to enable autoformat
+vim.api.nvim_create_user_command('FormatEnable', function()
+  vim.b.disable_autoformat = false
+  vim.g.disable_autoformat = false
+end, {
+  desc = 'Re-enable autoformat-on-save',
+})
--- lua/plugins/format.lua
+++ lua/plugins/format.lua
@@ -7,7 +7,13 @@ return {
       formatters_by_ft = {
         lua = { 'stylua' },
       },
-      format_on_save = { timeout_ms = 500, lsp_fallback = true },
+      format_on_save = function(bufnr)
+        -- Disable with a global or buffer-local variable
+        if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+          return
+        end
+        return { timeout_ms = 500, lsp_format = 'fallback' }
+      end,
     },
   },
 }
