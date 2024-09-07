9bfbff7fa487aa737638910c30e76212774240d9
Tharindu Pramuditha <tharindutpk@gmail.com>
2024-09-07 07:02:18 +0530

    Added conform disable option on filetype

--- lua/plugins/format.lua
+++ lua/plugins/format.lua
@@ -34,7 +34,18 @@ return {
         if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
           return
         end
-        return { timeout_ms = 500, lsp_format = 'fallback' }
+
+        -- Disable "format_on_save lsp_fallback" for languages that don't
+        -- have a well standardized coding style. You can add additional
+        -- languages here or re-enable it for the disabled ones.
+        local disable_filetypes = { c = true, cpp = true }
+        local lsp_format_opt
+        if disable_filetypes[vim.bo[bufnr].filetype] then
+          lsp_format_opt = 'never'
+        else
+          lsp_format_opt = 'fallback'
+        end
+        return { timeout_ms = 500, lsp_format = lsp_format_opt }
       end,
     },
   },
