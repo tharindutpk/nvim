a7503c2a094cf889f5af305d04a792bef9edc343
Tharindu Pramuditha <tharindutpk@gmail.com>
2024-12-31 20:51:39 +0530

    Add lazy.nvim and basic keymaps

---
+++ init.lua
@@ -0,0 +1,15 @@
+-- Set <space> as the leader key
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- [[ Setting options ]]
+require('config.options')
+
+-- [[ Basic Keymaps ]]
+require('config.keymaps')
+
+-- [[ Install lazy.nvim plugin manager ]]
+require('config.lazy')
+
+-- The line beneath this is called `modeline`. See `:help modeline`
+-- vim: ts=2 sts=2 sw=2 et
