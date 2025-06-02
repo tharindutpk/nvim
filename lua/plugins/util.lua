b658d6cbf09c9484fbd275f79af0091d0d7639b1
Tharindu Pramuditha <tharindutpk@gmail.com>
2025-06-02 23:45:24 +0530

    Add lspsaga

--- lua/plugins/util.lua
+++ lua/plugins/util.lua
@@ -4,4 +4,15 @@ return {
   { 'nvim-lua/plenary.nvim', lazy = true },
 
   { 'dstein64/vim-startuptime', cmd = 'StartupTime' },
+
+  {
+    'nvimdev/lspsaga.nvim',
+    config = function()
+      require('lspsaga').setup({})
+    end,
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter',
+      'nvim-tree/nvim-web-devicons',
+    },
+  },
 }
