31a850cadac18082e227c4d080d4576dd51bc263
Tharindu Pramuditha <tharindutpk@gmail.com>
2025-10-13 14:51:22 +0530

    Change mason config to align with v2

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -234,6 +234,20 @@ return {
       --  You can press `g?` for help in this menu.
       require('mason').setup()
 
+      for server_name, server_config in pairs(servers) do
+        server_config.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server_config.capabilities or {})
+        -- This handles overriding only values explicitly passed
+        -- by the server configuration above. Useful when disabling
+        -- certain features of an LSP
+        if server_name == 'rust_analyzer' then
+          return
+        elseif server_name == 'ruff' then
+          server_config.capabilities.hoverProvider = true
+        end
+        vim.lsp.config(server_name, server_config)
+        vim.lsp.enable(server_name)
+      end
+
       -- You can add other tools here that you want Mason to install
       -- for you, so that they are available from within Neovim.
       local ensure_installed = vim.tbl_keys(servers or {})
@@ -247,29 +261,6 @@ return {
         'markdownlint',
       })
       require('mason-tool-installer').setup({ ensure_installed = ensure_installed })
-
-      require('mason-lspconfig').setup({
-        ensure_installed = {},
-        automatic_installation = true,
-        handlers = {
-          function(server_name)
-            local server = servers[server_name] or {}
-            -- This handles overriding only values explicitly passed
-            -- by the server configuration above. Useful when disabling
-            -- certain features of an LSP (for example, turning off formatting for ts_ls)
-            server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
-            if server_name ~= nil then
-              if server_name == 'rust_analyzer' then
-                return
-              elseif server_name == 'ruff' then
-                -- disable hover in favor of pyright
-                server.capabilities.hoverProvider = false
-              end
-            end
-            require('lspconfig')[server_name].setup(server)
-          end,
-        },
-      })
     end,
   },
 }
