8aefb98fdd3b3dbb48f05cba7144e3977af5b234
Tharindu Pramuditha <git@tharindu.co>
2026-02-06 14:28:53 +0530

    Contain conform settings inside the plugin

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -4,21 +4,23 @@ local conform = require('conform')
 
 conform.setup({
   format_on_save = function(bufnr)
-    -- Disable with a global or buffer-local variable
+    -- Global or buffer-local disable
     if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
       return
     end
 
-    -- Disable "format_on_save lsp_fallback" for languages that don't
-    -- have a well standardized coding style. You can add additional
-    -- languages here or re-enable it for the disabled ones.
-    local disable_filetypes = { c = true, cpp = true }
+    -- Disable LSP fallback for non-standardized styles
+    local disable_filetypes = {
+      lua = true,
+    }
+
     local lsp_format_opt
     if disable_filetypes[vim.bo[bufnr].filetype] then
       lsp_format_opt = 'never'
     else
       lsp_format_opt = 'fallback'
     end
+
     return {
       lsp_format = lsp_format_opt,
       timeout_ms = 500,
@@ -48,4 +50,25 @@ vim.keymap.set({ 'n', 'v' }, '<leader>fb', function()
     async = true,
     lsp_format = 'fallback',
   })
-end)
+end, {
+  desc = 'Format buffer or selection',
+})
+
+-- User commands: enable / disable autoformat
+vim.api.nvim_create_user_command('FormatDisable', function(args)
+  if args.bang then
+    vim.b.disable_autoformat = true
+  else
+    vim.g.disable_autoformat = true
+  end
+end, {
+  desc = 'Disable autoformat on save',
+  bang = true,
+})
+
+vim.api.nvim_create_user_command('FormatEnable', function()
+  vim.b.disable_autoformat = false
+  vim.g.disable_autoformat = false
+end, {
+  desc = 'Enable autoformat on save',
+})
