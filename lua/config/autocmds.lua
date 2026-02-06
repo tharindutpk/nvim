dc275e10c2bcf1ec72cc0f7b6fef12aaa4bf719d
Tharindu Pramuditha <git@tharindu.co>
2026-02-06 14:30:23 +0530

    Remove conform settings

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -14,25 +14,6 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
--- format
-vim.api.nvim_create_user_command('FormatDisable', function(args)
-  if args.bang then
-    vim.b.disable_autoformat = true
-  else
-    vim.g.disable_autoformat = true
-  end
-end, {
-  desc = 'Disable autoformat on save',
-  bang = true,
-})
-
-vim.api.nvim_create_user_command('FormatEnable', function()
-  vim.b.disable_autoformat = false
-  vim.g.disable_autoformat = false
-end, {
-  desc = 'Enable autoformat on save',
-})
-
 -- buffers
 vim.api.nvim_create_autocmd('BufReadPost', {
   group = augroup('last_location'),
